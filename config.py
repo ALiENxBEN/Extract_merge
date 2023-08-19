11c8e096c00e1cc1a175c1551e62653c26883a0c
ZxScripter <131220168+ZxScripter@users.noreply.github.com>
2023-08-19 06:47:39 +0530

    config.py

--- config.py
+++ config.py
@@ -2,14 +2,14 @@ import os
 
 
 class Config(object):
-    API_HASH = os.environ.get("API_HASH")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN")
-    TELEGRAM_API = os.environ["TELEGRAM_API"]
-    OWNER = os.environ.get("OWNER")
-    OWNER_USERNAME = os.environ.get("OWNER_USERNAME")
-    PASSWORD = os.environ.get("PASSWORD")
-    DATABASE_URL = os.environ.get("DATABASE_URL")
-    LOGCHANNEL = os.environ.get("LOGCHANNEL")  # Add channel id as -100 + Actual ID
+    API_HASH = os.environ.get("1f5343b0646645ca1eaf7c4759fc248f")
+    BOT_TOKEN = os.environ.get("6619594862:AAF7IMrryWmLpLnKDi-s8J41_u8YvbQHxm8")
+    TELEGRAM_API = os.environ.get("26376042")
+    OWNER = os.environ.get("2036803347")
+    OWNER_USERNAME = os.environ.get("Sensei_Rimuru")
+    PASSWORD = os.environ.get("AllInOne")
+    DATABASE_URL = os.environ.get("mongodb+srv://userbot:userbot@cluster0.ltasu.mongodb.net/myFirstDatabase?retryWrites=true&w=majority")
+    LOGCHANNEL = os.environ.get("-1001934076980")  # Add channel id as -100 + Actual ID
     GDRIVE_FOLDER_ID = os.environ.get("GDRIVE_FOLDER_ID","root")
     USER_SESSION_STRING = os.environ.get("USER_SESSION_STRING", None)
     IS_PREMIUM = False
